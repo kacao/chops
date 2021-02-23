56b0d8b2e9fc5e9a04fc90e2443e13fd24051bb4
Khanh Cao <khanhncao@gmail.com>
2021-02-22 16:32:31 -0800

    .

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -15,9 +15,17 @@ export declare class Logger {
     fields: Record<string, FieldRecord>;
     constructor(fields?: Fields);
     withFields(fields: Fields): this;
-    log(msg: string): void;
+    log(msg: string, level?: string): void;
+    debug(msg: string): void;
+    info(msg: string): void;
+    warn(msg: string): void;
+    error(msg: string): void;
 }
-export declare function log(msg: string): void;
-export declare function addFields(fields: Fields): Logger;
+export declare function log(msg: string, level?: string): void;
+export declare function withFields(fields: Fields): Logger;
 export declare function getLogger(): Logger;
+export declare var debug: (msg: string) => void;
+export declare var info: (msg: string) => void;
+export declare var warn: (msg: string) => void;
+export declare var error: (msg: string) => void;
 export {};
